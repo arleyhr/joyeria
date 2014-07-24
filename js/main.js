905745a1750e09ec2d0c5252c23996167743d688
arleyhr <arley.hr@hotmail.com>
2014-07-23 23:21:58 -0500

    Contacto

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ $(function(){
 		else
 			m.pause();
 	});
+	$("#contacto input").focus(function(e){
+		console.log(e.target);
+		alert("yeah");
+	});
 });
 
 var app = angular.module('app',['ngRoute','ngResource','ngAnimate']);
